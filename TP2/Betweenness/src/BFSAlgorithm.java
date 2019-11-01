14b482e201ec7a7800c36d8e999d55d67766ab0f
Administrateur <lilya.m@live.fr>
2019-11-01 20:42:30 +0100

    calcul distance + début npcc

--- TP2/Betweenness/src/BFSAlgorithm.java
+++ TP2/Betweenness/src/BFSAlgorithm.java
@@ -1,5 +1,7 @@
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Vector;
@@ -18,50 +20,47 @@ public class BFSAlgorithm {
     public BFSAlgorithm(Graphe graphe) {
         this.graphe = graphe;
     }
+    
+    public void BFS (int u) {    	
+    	HashMap <Integer, String> color = new HashMap<Integer, String>();
+    	for (int i = 0; i<=graphe.getNbr_sommet(); i++)  color.put(i, "blanc");
+    	
+    	Deque <Integer> file = new LinkedList<>();
+    	file.add(u);
+    	color.replace(u, "gris");
+    	int s;
+    	
+    	while(!file.isEmpty()) {
+    		s = file.removeFirst();
+    		System.out.println("sommet courant : " + s);
+    		for(Sommet tmp : graphe.getSommetByID(s).getAdjacence()) {
+    			String col = color.get(tmp.getId());
+    			if(! (col.equals("noir"))) {
+    				//si première visite ajouter à la file et changer la couleur
+    				if(color.get(tmp.getId()).equals("blanc")) {
+    					color.replace(tmp.getId(), "gris");
+        					distance_matrice[u][tmp.getId()] = distance_matrice[u][s] + 1;
+    					file.add(tmp.getId());
+    				}
+    			}
+    			if(s == u) {
+    				distance_matrice[u][tmp.getId()] = 1;
+    				npcc[u][tmp.getId()] = 1;
+    			}
+    			else if(tmp.getId() != s && tmp.getId() != u) {
+    				int dist = distance_matrice[u][s] + npcc[s][tmp.getId()];
+    				if(tmp.getAdjacence().size() == 1) npcc[u][tmp.getId()] = npcc[u][s];
+    				if(distance_matrice[u][tmp.getId()] == 1) npcc[u][tmp.getId()] = 1;
+    					
+    				else if(distance_matrice[u][tmp.getId()] == dist) npcc[u][tmp.getId()] += 1;
+    				else  npcc[u][tmp.getId()] += npcc[u][s];
+    			}
+    		}
+    		color.replace(s, "noir");
+    	}
+    }
         
-    // Method for finding minimum no. of edge 
-    // using BFS 
-    public int minEdgeBFS(int u, int v, int n) { 
-        // visited[n] for keeping track of visited 
-        // node in BFS 
-        ArrayList<Boolean> visited = new ArrayList<Boolean>(n); 
-          
-        for (int i = 0; i < n; i++) { 
-            visited.add(false); 
-        } 
-       
-        // Initialize distances as 0 
-        ArrayList<Integer> distance = new ArrayList<Integer>(n); 
-          
-        for (int i = 0; i < n; i++) { 
-            distance.add(0); 
-        } 
-       
-        // queue to do BFS. 
-        Queue<Integer> Q = new LinkedList<>(); 
-        //distance.setElementAt(0, u);
-        distance.set(u, 0);
-       
-        Q.add(u); 
-        //visited.setElementAt(true, u);
-        visited.set(u, true);
-        while (!Q.isEmpty()){ 
-            int x = Q.peek(); 
-            Q.poll(); 
-            Sommet som = graphe.getSommetByID(x);
-            for (int i=0; i<som.getAdjacence().size(); i++){ 
-                if (visited.get(som.getSommetAdjacentByIndex(i).getId())) continue; 
-                    
-                // update distance for i 
-                //distance.setElementAt(distance.get(x) + 1, som.getSommetAdjacentByIndex(i).getId());
-                distance.set(som.getSommetAdjacentByIndex(i).getId(), distance.get(x) + 1);
-                Q.add(som.getAdjacence().get(i).getId()); 
-                //visited.setElementAt(true,som.getSommetAdjacentByIndex(i).getId());
-                visited.set(som.getSommetAdjacentByIndex(i).getId(), true);
-            } 
-        } 
-        return distance.get(v); 
-    } 
+     
     
     void addBet() {
     	int size = graphe.getSommets().size();
@@ -115,13 +114,29 @@ public class BFSAlgorithm {
     }
     
     void addDistance() {
+    	System.out.println("Dans addDistance");
     distance_matrice = new int[graphe.getSommets().size()][graphe.getSommets().size()];
-		for (int i = 1; i < graphe.getSommets().size(); i++) {
-	        for(int j = 1; j < i+1; j++) {
-	            distance_matrice[i][j] =  minEdgeBFS(i, j, graphe.getSommets().size());
-	            System.out.print(distance_matrice[i][j]+" ");
-	        }
-	        System.out.println();
+    npcc = new int[graphe.getSommets().size()][graphe.getSommets().size()];
+
+		for (int i = 0; i <= graphe.getNbr_sommet(); i++) {
+			System.out.println("BFS sur : "+i);
+			BFS(i);
 	    }
-}
+		System.out.println("*********Matrice des distances*************");
+		for(int i = 0; i<=graphe.getNbr_sommet(); i++) {
+	       // System.out.println("col "+i+":");
+			for(int j= 0; j<=graphe.getNbr_sommet(); j++) {
+	            System.out.print(distance_matrice[i][j]+" ");
+			}
+	        System.out.println();
+		}
+		System.out.println("*********Matrice des npcc*************");
+		for(int i = 0; i<=graphe.getNbr_sommet(); i++) {
+	       // System.out.println("col "+i+":");
+			for(int j= 0; j<=graphe.getNbr_sommet(); j++) {
+	            System.out.print(npcc[i][j]+" ");
+			}
+	        System.out.println();
+		}
+    }
 }
