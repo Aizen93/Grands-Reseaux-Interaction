b82847533b19ea5517fc85be0f07e19f8ea18600
aouessar <oussama--93@live.fr>
2019-11-12 23:37:46 +0100

    More lisibility

--- TP3/Modularité/src/Graphe.java
+++ TP3/Modularité/src/Graphe.java
@@ -4,4 +4,28 @@ class Graphe {
     int degreMax;   // degre max d'un sommet
     int somdmax;   // numero du premier sommet atteignant le degré maximum
     Sommet[] sommets; // tableau des sommets. De taille n+1 normalement
+    
+    public void generateGraphe(String path){
+        Parser parser = new Parser();
+	parser.lecture(this, path);
+    }
+    
+    public void printResult(){
+        // 2- affichages
+	System.out.println("Nombre de sommets : "+(nbr_sommet));
+	System.out.println("Nombre d'aretes : "+nbr_arete);
+	System.out.println("Sommet de degré max (de numéro minimal) : " + somdmax);
+	System.out.println("Sa liste d'adjacence (ligne suivante) :");
+        
+ 	for(int i=0; i< sommets[somdmax].degre; i++) 
+	    System.out.print(sommets[somdmax].adjacence[i]+ " ");
+        
+	System.out.println("\nDitribution des degrés : ");
+	// calculs de degré
+	int[] dgr = new int[degreMax + 1];
+	for(int i = 0; i < sommets.length; i++)
+	    dgr[ sommets[i].degre ]++;
+	for(int i = 0; i <= degreMax; i++) 
+	    System.out.println(i + " " + dgr[i]);
+    }
 }
--- TP3/Modularité/src/TP3.java
+++ TP3/Modularité/src/TP3.java
@@ -8,27 +8,10 @@ public class TP3 {
 	    return;
 	}
 	
-	Graphe graphe = new Graphe(); // le graphe sur lequel on travaille 
-        Parser parser = new Parser();
-	// 1- lecture
-	parser.lecture(graphe, args[0]);
+	Graphe graphe = new Graphe(); // le graphe sur lequel on travaille
+        graphe.generateGraphe(args[0]);
+        graphe.printResult();
 
-	// 2- affichages
-	System.out.println("Nombre de sommets : "+(graphe.nbr_sommet));
-	System.out.println("Nombre d'aretes : "+graphe.nbr_arete);
-	System.out.println("Sommet de degré max (de numéro minimal) : " + graphe.somdmax);
-	System.out.println("Sa liste d'adjacence (ligne suivante) :");
-        
- 	for(int i=0; i<graphe.sommets[graphe.somdmax].degre; i++) 
-	    System.out.print(graphe.sommets[graphe.somdmax].adjacence[i]+ " ");
-        
-	System.out.println("\nDitribution des degrés : ");
-	// calculs de degré
-	int[] dgr = new int[graphe.degreMax + 1];
-	for(int i = 0; i < graphe.sommets.length; i++)
-	    dgr[ graphe.sommets[i].degre ]++;
-	for(int i = 0; i <= graphe.degreMax; i++) 
-	    System.out.println(i + " " + dgr[i]);
         
 	System.out.println("Mémoire allouée : " + (Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory()) + " octets");
     }
